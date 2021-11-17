6c2b08a2acbb1ca2e53bd64f882920902349b18c
YoannKersulec <yoann.kersulec@epitech.eu>
2021-11-17 19:59:50 +0100

    Grid and value for the game

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -4,6 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    
+    var currentPlayer = 0
+    var gameState: IntArray = intArrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2)
+
+    var winPositions = arrayOf(
+        intArrayOf(0, 1, 2),
+        intArrayOf(3, 4, 5),
+        intArrayOf(6, 7, 8),
+        intArrayOf(0, 3, 6),
+        intArrayOf(1, 4, 7),
+        intArrayOf(2, 5, 8),
+        intArrayOf(0, 4, 8),
+        intArrayOf(2, 4, 6)
+    )
+    var pressCounter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
